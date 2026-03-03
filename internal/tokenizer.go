418131abede3f2a7d2deef40feaa8033af3ebdb4
emoduh <eoduh22@gmail.com>
2026-03-03 06:51:06 +0000

    commiit to update repo

--- internal/tokenizer.go
+++ internal/tokenizer.go
@@ -1,1 +1,30 @@
 package internal
+
+func Tokenize(text string) []string {
+	var result []string
+	runes := []rune(text)
+	sep := rune(' ')
+	coma := rune(',')
+	dot := rune('.')
+	quem := rune('!')
+	opp := rune('(')
+	word := ""
+	IsInsideBrackecks := false
+
+	for i := 0; i < len(runes)-1; i++ {
+		char := runes[i]
+		if char == opp {
+			IsInsideBrackecks = true
+		}
+		switch {
+		case IsInsideBrackecks:
+			word += string(char)
+		case char == ')':
+			result = append(result, word)
+			word = ""
+			IsInsideBrackecks = false
+		default:
+			word += string(char)
+		}
+	}
+}
