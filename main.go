c31f5a1ab65e9c31cb6345b6135819c934fe9e0a
emoduh <eoduh22@gmail.com>
2026-03-05 18:05:46 +0000

    changes made todya

--- main.go
+++ main.go
@@ -10,13 +10,18 @@ func main() {
 	//a := "hello, world! (up, 2) hello !!! (up, 2);;; hello(up) ' awesome ' ... Simply add 42 (hex) and 10 (bin)"
 	//c := processor.Tokenize(a)
 	//fmt.Println(c)
-	tokens := []string{"ema", "(up)", "and", "ff", "(up)"}
+	tokens := []string{"come", "ema", "and", "ff", "(up, 2)"}
 	for i := 0; i < len(tokens); i++ {
-		if tokens[i] == "(up)" && i > 0 {
-			str := tokens[i-1]
-			str = strings.ToUpper(str)
-			tokens[i-1] = str
-			tokens[i] = ""
+		if tokens[i] == "(up, 2)" && i > 0 {
+			for t := i - 1; t >= 0; t-- {
+				str := tokens[t]
+				str = strings.ToUpper(str)
+				tokens[t] = str
+				if t < 2 {
+					break
+				}
+			}
+			tokens = append(tokens[:i], tokens[i+1:]...)
 		}
 	}
 	fmt.Println(tokens)
--- processor/modifiers.go
+++ processor/modifiers.go
@@ -1,28 +1,52 @@
 package processor
 
 import (
+	"fmt"
 	"strconv"
+	"strings"
 )
-/*
-for i := 0; i < len(tokens); i++ {
-    if tokens[i] == "(hex)" {
-        // modify tokens[i-1]
-        // remove tokens[i]
-        tokens = append(tokens[:i], tokens[i+1:]...)
-        i-- // IMPORTANT
-    }
-}
-	*/
 
 func ApplyModifiers(token []string) []string {
 	for i := 0; i < len(token); i++ {
 		switch {
-		case token[i] == ("hex"):
+		case token[i] == "(hex)" && i < 0:
 			num, err := strconv.ParseInt(token[i-1], 16, 64)
 			if err != nil {
 				continue
 			}
-			token[i-1] = 
+			token[i-1] = fmt.Sprintf("%d", num)
+			token = append(token[:i], token[i+1:]...)
+		case token[i] == "(bin)" && i > 0:
+			num, err := strconv.ParseInt(token[i-1], 2, 64)
+			if err != nil {
+				continue
+			}
+			token[i-1] = fmt.Sprintf("%d", num)
+			token = append(token[:i], token[i+1:]...)
+		case token[i] == "(up)" && i < 0:
+			token[i-1] = strings.ToUpper(token[i-1])
+			token = append(token[:i], token[i+1:]...)
+		case token[i] == "(low)" && i < 0:
+			token[i+1] = strings.ToLower(token[i+1])
+			token = append(token[:i], token[i+1:]...)
+		case token[i] == "(cap)" && i > 0:
+			rstr := []rune(token[i-1])
+			if rstr[0] >= 'a' && rstr[0] <= 'z' {
+				rstr[0] = rstr[0] - 32
+				token[i-1] = string(rstr)
+			}
+			token = append(token[:i], token[i+1:]...)
+		case token[i] == ("up, 2"):
+			for t := i - 1; t >= 0; t-- {
+				str := token[t]
+				str = strings.ToUpper(str)
+				token[t] = str
+				if t < 2 {
+					break
+				}
+			}
+			token = append(token[:i], token[i+1:]...)
 		}
 	}
+	return token
 }
