76eaccd98bfd9b259586dee488626b3ceefefe26
emoduh <eoduh22@gmail.com>
2026-03-10 21:54:00 +0000

    reupdate

--- sample_test.go
+++ sample_test.go
@@ -1,10 +1,34 @@
 package main
 
-/*
 import (
+	"go-reloaded/processor"
+	//"reflect"
 	"testing"
 )
 
-func TestFixArticles(t *testing.T) {
+func equalSlice(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, _ := range b {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
 
-*/
+func TestFixArticles(t *testing.T) {}
+
+func TestTokenize(t *testing.T) {
+	want := []string{"hello", "...", "'", "world", "'"}
+	got := processor.Tokenize("hello ... ' world '")
+
+	if !equalSlice(got, want) {
+		t.Errorf("got: %v want: %v", got, want)
+	}
+}
+
+func TestFixApplyModifiers(t *testing.T) {}
+
+func TestRebuild(t *testing.T) {}
