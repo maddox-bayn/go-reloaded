22b884649d71afb32e7ede0e16d6c0f9063383ac
emoduh <eoduh22@gmail.com>
2026-03-14 13:57:47 +0000

    to finish before recode

--- processor/articles.go
+++ processor/articles.go
@@ -4,8 +4,6 @@ import "strings"
 
 func FixArticles(tokens []string) []string {
 
-	//vowels := map[rune]bool{'a': true, 'e': true, 'i': true, 'u': true, 'o': true, 'h': true, 'A': true, 'E': true, 'I': true, 'O': true, 'U': true, 'H': true}
-
 	for i := 0; i < len(tokens); i++ {
 
 		if (tokens[i] == "a" || tokens[i] == "A") && i < len(tokens) {
