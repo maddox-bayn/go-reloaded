07c3a71eed56bb176f3273f1b6ee062c905b344c
Oduh Emmanuel Aba <eoduh22@gmail.com>
2026-06-26 11:12:48 +0100

    Enhance main.go with comments for clarity
    Added comments to clarify the purpose of code sections.

--- main.go
+++ main.go
@@ -7,21 +7,22 @@ import (
 	"strings"
 )
 
+// engine and caller of the functions
 func main() {
+	// validat args and get file names
 	if len(os.Args) != 3 {
 		fmt.Println("Usage: go run . sample.txt, result.txt")
 		return
 	}
-
 	inputfile := os.Args[1]
 	outputfile := os.Args[2]
-
+	// read the input file content and return it as byte
 	data, err := os.ReadFile(inputfile)
 	if err != nil {
 		fmt.Println("Error reading file", err)
 		return
 	}
-
+	// process file content through the processor
 	text := string(data)
 
 	token := processor.Tokenize(text)
@@ -32,7 +33,7 @@ func main() {
 	if !strings.HasSuffix(result, "\n") {
 		result += "\n"
 	}
-
+	// write processed text to the output file
 	err = os.WriteFile(outputfile, []byte(result), 0644)
 
 	if err != nil {
